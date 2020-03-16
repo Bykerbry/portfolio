fd18778234adff2c47674ce288ba4ff58895dcc2
Bryce <bykerbry@umich.edu>
2020-03-16 16:00:29 -0400

    add project section formatting

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,7 +11,7 @@ export class AppComponent {
   title = 'portfolio';
   routes: string[]= ['home', 'about', 'resume', 'contact'];
   opened: any; 
-  mobile: Observable<BreakpointState> = this.breakpointObserver.observe(Breakpoints.Handset)
+  mobile: Observable<BreakpointState> = this.breakpointObserver.observe(Breakpoints.HandsetPortrait)
   // Below 600px is good. @601px to 635px is desktop. From 636px to 960px its mobile again...
   constructor(private breakpointObserver: BreakpointObserver) { 
   }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -12,24 +12,28 @@ export class HomeComponent implements OnInit {
     {
       title: "Recipe Search",
       desc: "Using git to collaborate, my team & I built a web app in Angular 8 that utilizes Edamam's Recipe Search API. We made use of Angular Material for many of the inputs, buttons, icons & dialogs. I was responsible for creating the API request, implementing/displaying filters and formatting the results in the Home page.",
+      icon: "restaurant",
       codeURL: "https://github.com/Bykerbry/recipe-api-project",
       appURL: "https://bykerbry.github.io/recipe-api-project/"
     },
     {
       title: "Budget Buddy",
       desc: "Using vanilla JavaScript, HTML & CSS, my team & I built a budgeting app. The app is mobile & desktop responsive and we used git for collaboration. A clean & simple user interface walks the user through entering their income & expenses, breaks down the numbers into a weekly budget and generates an expense report.",
+      icon: 'account_balance',
       codeURL: "https://github.com/ottokeres/Budget-Buddy/",
       appURL: "https://bykerbry.github.io/Budget-Buddy/"
     },
     {
       title: "",
       desc: "",
+      icon: '',
       codeURL: "",
       appURL: ""
     },
     {
       title: "",
       desc: "",
+      icon: '',
       codeURL: "",
       appURL: ""
     },
