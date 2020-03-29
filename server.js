1d79acfad87f28b6603e62b79561fdb25c740c64
Bryce <bykerbry@umich.edu>
2020-03-29 18:27:36 -0400

    Add node.js backend from which to serve app

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express()
+const port = process.env.PORT || 3000;
+const path = require('path')
+
+app.use(express.static('dist/portfolio'))
+
+app.get('/', (req, res) => {
+    let options = {
+        root: path.join(__dirname, 'dist/portfolio')
+    }
+    return res.sendFile('index.html', options)
+})
+
+
+app.listen(port, () => {
+    console.log('App running on port ' + port);
+})
